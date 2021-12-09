4051948e9c1cffca414be94633429ba5ff15b08a
Petty <dobos.peter.2001@gmail.com>
2021-12-09 19:03:18 +0100

    quit gomb mukodik

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -4,14 +4,38 @@ import controller.Controller;
 import model.Game;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame{
         //public STATE state = STATE.MENU;
         public CheckerBoard cb;
         //public Menu menu;
+        public JPanel cards;
+
 
         public Frame(Game g, Controller c){
+
             //super();
+            JPanel menu = new JPanel();
+            JButton newGameButton = new JButton("New Game");
+            JButton quitButton = new JButton("Quit");
+            newGameButton.addActionListener(e -> {dispose();});
+            quitButton.addActionListener(e -> {dispose();});
+            menu.add(newGameButton);
+            menu.add(quitButton);
+
+            JPanel game = new JPanel();
+            cb = new CheckerBoard();
+            cb.setGame(g);
+            cb.addMouseListener(c);
+            getContentPane().add(cb);
+            game.add(cb);
+
+            cards = new JPanel(new CardLayout());
+            cards.add(menu);
+            cards.add(game);
+            add(cards);
+
             setSize(800,800);
             setTitle("Checkers");
             setLocationRelativeTo(null);
@@ -19,12 +43,12 @@ public class Frame extends JFrame{
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-           // if(state==STATE.GAME){
+           /* if(state==STATE.GAME){
                 cb = new CheckerBoard();
                 cb.setGame(g);
                 cb.addMouseListener(c);
                 getContentPane().add(cb);
-            //}
+            }*/
            /* if(state==STATE.MENU) {
                 menu = new Menu();
                 menu.addMouseListener(c);
